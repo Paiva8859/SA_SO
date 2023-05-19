7607e4de9483792bd5094568d42d59c91c211ab3
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-05-19 12:57:35 +0000

    Arrumando verifyCampo

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
-//Dados
+//-----==Dados==-----//
+//Links sem acesso
 const block = document.querySelector(".block");
-const logar = document.querySelector(".logar");
+//Formulário Todo
 const form = document.querySelector(".formLogin");
+//Enviar formulário (Submit)
+const logar = document.querySelector(".logar");
+//Aviso de verificação de campos
 const avisoE = document.querySelector(".avisoE");
 const avisoS = document.querySelector(".avisoS");
+//Campos
+const userInput = document.getElementById("user");
+const passwordInput = document.getElementById("password");
+//Array que guarda Usuários
 var formulario = [{
   user: 'adm@adm',
   password: 'admin'
 }];
+//Usuário atualmente logado
 var usuarioAtual;
 
-//Events
+//-----==Events==-----//
+//Usuário sem permissão tentando acessar os links 'proibidos'
 block.addEventListener("click", bloqueio);
-logar.addEventListener("click", verifyCampo);
+//Verificar se o usuário já esta logado
+logar.addEventListener("click", verifyLogado);
+//verificar se campo email está certo
+userInput.addEventListener("blur", verifyCampoUser);
+//verificar se campo senha está certo
+passwordInput.addEventListener("blur", verifyCampoPassword);
 
-//Functions
+//-----==Functions==-----//
 function verifyLogado(event){
   event.preventDefault();
   
@@ -39,24 +54,18 @@ function verifyLogado(event){
   }
 }
 
-function verifyCampo(event){
-  event.preventDefault();
-
+function verifyCampoUser(){
   var usuario = document.getElementById("user").value;
+
+  if (usuario.length < 3 || !usuario.includes("@")) {
+    avisoE.classList.toggle("aparece");
+  }
+}
+function verifyCampoUser(){
   var senha = document.getElementById("password").value;
 
-  if (usuario.length < 3 || usuario.indexOf("@") === -1 && senha.length < 4) {
-    avisoE.classList.toggle("aparece");
+  if (senha.length < 4) {
     avisoS.classList.toggle("aparece");
-  }
-  else if(usuario.length < 3 || usuario.indexOf("@") === -1 && senha.length >= 4){
-    avisoE.classList.toggle("aparece");
-  }
-  else if(usuario.length >= 3 || usuario.indexOf("@") !== -1 && senha.length < 4){
-    avisoS.classList.toggle("aparece");
-  }
-  else{
-    verifyLogado();
   }
 }
 
