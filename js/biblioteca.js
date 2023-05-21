fa003a961fd8975884c39e27171fd36feafa394e
Lorenzo O. Zimbres <lorenzozimbresfilmmaker@gmail.com>
2023-05-20 22:41:33 -0300

    Biblioteca finalizada

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -9,12 +9,16 @@ cabecalhoGen.forEach((section) => {
 
 //-----==Funções==-----//
 function abreLista() {
-    this.querySelector(".lista").classList.toggle("show");
-
-    cabecalhoGen.forEach((section) => {
-        if (section !== this) {
-            const listaNaoPassada = section.querySelector(".lista");
-            listaNaoPassada.classList.toggle("show");
-        }
-    });
+    const listaClicada = this.querySelector(".lista");
+  
+  // Remover a classe "show" de todas as listas, exceto da lista clicada
+  cabecalhoGen.forEach((section) => {
+    const lista = section.querySelector(".lista");
+    if (lista !== listaClicada) {
+      lista.classList.remove("show");
+    }
+  });
+  
+  // Alternar a classe "show" apenas na lista clicada
+  listaClicada.classList.toggle("show");
 }
